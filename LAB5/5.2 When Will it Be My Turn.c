fc9351815e99c12177e2e5edd769d5ab419a3351
Rewwiw <winner2547.wn@gmail.com>
2024-02-22 10:53:41 +0700

    a

--- LAB5/5.2 When Will it Be My Turn.c
+++ LAB5/5.2 When Will it Be My Turn.c
@@ -1,40 +1,29 @@
 //66070503416 Thapana Liamthongkaow
 //Lab 5.2 When Will It Be  My Turn
 #include<stdio.h>
-#include<string.h>
 
-void insertq(int *queue, int val);
-void dequeue(int *queue);
-void show(int *queue);
-
-int front = -1, rear = -1;
-int max_size = 0;
+void enQ(int *queue, int *front, int *rear, int maxsize);
+void deQ(int *queue, int *front, int *rear, int maxsize);
+void show(int *queue, int *front, int *rear, int maxsize);
 
 int main(){
-    int val;
     char option;
-    scanf("%d",&max_size);
-    if(max_size<1 || max_size>10){
+    int maxsize, front=-1, rear=-1;
+    scanf("%d",&maxsize);
+    if(maxsize<1 || maxsize>10){
         return 0;
     }
-    int queue[max_size];
-
+    int queue[maxsize];
     while(scanf(" %c",&option)==1){
         switch(option){
             case 'I':
-                scanf("%d",&val);
-                if(front != (rear + 1)%max_size){
-                    insertq(queue, val);
-                }
-                else{
-                    printf("Queue is full!!\n");
-                }
+                enQ(queue, &front, &rear, maxsize);
                 break;
             case 'D':
-                dequeue(queue);
+                deQ(queue, &front, &rear, maxsize);
                 break;
             case 'S':
-                show(queue);
+                show(queue, &front, &rear, maxsize);
                 break;
             case 'E':
                 return 0;
@@ -44,57 +33,49 @@ int main(){
     }
 }
 
-void insertq(int *queue, int val){
-    if(front == (rear + 1) % max_size){
+void enQ(int *queue, int *front, int *rear, int maxsize){
+    // printf("[enqueue]\n");
+    if(*front == (*rear + 1) % maxsize){
         printf("Queue is full!!\n");
         return;
     }
-    else{
-        if(front == -1 && rear == -1){
-            front = rear = 0;
-            queue[rear] = val;    
-        }
-        else{
-            rear = (rear + 1) % max_size;
-            queue[rear] = val;
-        }
+    int val;
+    scanf("%d",&val);
+    if((*front)==-1 && (*rear)==-1){
+        (*front)=(*rear)=0;
     }
+    else{
+        (*rear) = (*rear + 1) % maxsize;
+    }
+    queue[*rear] = val;
 }
 
-void dequeue(int *queue){
-    int val = queue[front];
-    if(front == -1 && rear == -1){
+void deQ(int *queue, int *front, int *rear, int maxsize){
+    // printf("[dequeue]\n");
+    if((*front)==-1 && (*rear)==-1){
         printf("Queue is empty!!\n");
         return;
     }
-    else{
-        if(front == rear){
-            front = rear = -1;
-        }
-        else{
-            front = (front + 1) % max_size;
-        }
-        printf("%d\n",val);
+    int val_front = queue[*front];
+    if((*front)==(*rear)){
+        (*front)=(*rear)=-1;
     }
+    else{
+        (*front) = (*front+1) % maxsize;
+    }
+    printf("%d\n",val_front);
 }
 
-void show(int *queue){
-    if(front == -1 && rear == -1){
+void show(int *queue, int *front, int *rear, int maxsize){
+    // printf("[show]\n");
+    if((*front)==-1 && (*rear)==-1){
         printf("Queue is empty!!\n");
         return;
     }
-    else{
-        printf("Front: %d\n",front);
-        printf("Items: ");
-        if(front != -1 && rear != -1 && front == rear){
-            printf("%d",queue[front]);
-        }
-        else{
-            for(int i=front;i!=rear;i = (i+1) % max_size){
-                printf("%d ",queue[i]);
-            }
-            printf("%d",queue[rear]);
-        }
-        printf("\nRear: %d\n",rear);
+    int temp_front = *front, temp_rear = *rear;
+    printf("Front: %d\nItems: ",temp_front);
+    for(int i=temp_front;i!=(*rear);i=(i+1)%maxsize){
+        printf("%d ",queue[i]);
     }
+    printf("%d \nRear: %d\n",queue[temp_rear], temp_rear);
 }
--- LAB5/5.3 Secret Code Only You and I Know.c
+++ LAB5/5.3 Secret Code Only You and I Know.c
@@ -1,2 +1,93 @@
 //66070503416 Thapana Liamthongkaow
 //lab 5.3 Secret Code Only You and I Know
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+// Structures for queue implementation
+typedef struct node {
+    char data;
+    struct node *next;
+} node;
+
+typedef struct queue {
+    node *front;
+    node *rear;
+} queue;
+
+// Function prototypes
+int getPriority(char input);
+void enQ(node *newnode, queue *current);
+void print(queue *current);
+queue *classify(char *input, int checkPriority);
+node *createnode(char val);
+
+int main() {
+    char input[1001];
+    scanf("%[^\n]s", input);
+
+    queue *vowel_Q = classify(input, 1); // vowels
+    queue *conso_Q = classify(input, 3); // consonants
+    queue *other_Q = classify(input, 2); // others
+
+    print(vowel_Q);
+    print(other_Q);
+    print(conso_Q);
+
+    return 0;
+}
+
+int getPriority(char input) {
+    if (input == 'a' || input == 'A' || input == 'e' || input == 'E' || input == 'i' || input == 'I' || input == 'o' || input == 'O' || input == 'u' || input == 'U') {
+        return 1;
+    } else if (isalpha(input)) {
+        return 3;
+    }
+    return 2;
+}
+
+void enQ(node *newnode, queue *current) {
+    if (current->front == NULL && current->rear == NULL) {
+        current->front = newnode;
+        current->rear = newnode;
+    } else {
+        current->rear->next = newnode;
+        current->rear = newnode;
+    }
+}
+
+void print(queue *current) {
+    if (current->front == NULL) {
+        return;
+    }
+    node *now = current->front;
+    while (now != NULL) {
+        printf("%c", now->data);
+        now = now->next;
+    }
+}
+
+node *createnode(char val) {
+    node *newnode = (node *) malloc(sizeof(node));
+    newnode->data = val;
+    newnode->next = NULL;
+    return newnode;
+}
+
+queue *classify(char *input, int checkPriority) {
+    queue *current = (queue *) malloc(sizeof(queue));
+    current->front = NULL;
+    current->rear = NULL;
+
+    node *newnode = NULL;
+
+    while (*input) {
+        int pri_o = getPriority(*input);
+        if (pri_o == checkPriority) {
+            newnode = createnode(*input);
+            enQ(newnode, current);
+        }
+        input++;
+    }
+    return current;
+}
